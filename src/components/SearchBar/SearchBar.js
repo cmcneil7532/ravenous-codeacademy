4cfdc19db415e9091e6f556e5fb2eec44446d078
Christian McNeil <christianmcneil@ChristiansMBP2.fios-router.home>
2022-09-05 18:03:54 -0400

    Add SearchBar constructor

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./SearchBar.css";
 
 const sortByOptions = {
-  "Best Match": "best_math",
+  "Best Match": "best_match",
   "Highest Rated": "rating",
   "Most Reviewed": "review_count",
 };
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "", location: "", sortBy: "best_match" };
+  }
   renderSortByOptions() {
     return Object.keys(sortByOptions).map((sortByOption) => {
       let sortByOptionValue = sortByOptions[sortByOption];
